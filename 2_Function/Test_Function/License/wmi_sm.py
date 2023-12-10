e654bb353d506f38c87eb8a10a7c6472447ed733
SeoJungHun <102637600+CaDiVae@users.noreply.github.com>
2023-12-11 01:41:26 +0900

    현장 테스트 완료

--- 2_Function/Test_Function/License/wmi_sm.py
+++ 2_Function/Test_Function/License/wmi_sm.py
@@ -1,5 +1,8 @@
 import wmi
+import tkinter as tk
+import os
 
+# CPU 정보 가져오기
 cpu_info = ""
 c = wmi.WMI()
 processors = c.Win32_Processor()
@@ -7,8 +10,7 @@ for processor in processors:
     cpu_info = processor.ProcessorId
     break
 
-print(cpu_info)
-
+# 드라이브 일련번호 가져오기
 drive = "C"
 c = wmi.WMI()
 logical_disks = c.Win32_LogicalDisk(DeviceID=drive + ":")
@@ -16,7 +18,17 @@ for disk in logical_disks:
     volume_serial = disk.VolumeSerialNumber
     break
 
-print(volume_serial)
-
+# unique_id 생성
 unique_id = cpu_info + volume_serial
-print(unique_id)
+
+# Tkinter 창 생성 및 unique_id 표시
+window = tk.Tk()
+label = tk.Label(window, text=unique_id)
+label.pack()
+
+# unique_id를 사용자의 문서 폴더에 txt 파일로 저장
+docs_path = os.path.join(os.path.expanduser('~'), 'Documents')
+with open(os.path.join(docs_path, 'unique_id.txt'), 'w') as f:
+    f.write(unique_id)
+
+window.mainloop()
